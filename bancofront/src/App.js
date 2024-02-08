06109726c7416e68f890e8b2495f5f94e68441da
Dell <Dell@DESKTOP-EKCSEO5>
2024-02-08 18:48:18 -0300

    Agregado boton, modulos de axios y de react-icons.

--- bancofront/src/App.js
+++ bancofront/src/App.js
@@ -1,9 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Boton } from "./components/boton";
+import * as APITransferencia from "./services/transferencia";
 import LoginForm from "./Components/LoginForm/LoginForm";
 
 function App() {
   return (
     <div>
-         <LoginForm />
+      <LoginForm />
+      <div>
+        <Boton
+          className="boton-grande"
+          accion={APITransferencia.getTransferencias(1)}
+          nombreAccion="Ver actividad"
+        />
+      </div>
     </div>
   );
 }
