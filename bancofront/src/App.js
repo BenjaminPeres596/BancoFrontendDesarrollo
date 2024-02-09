d74308aba6f009f4a3d0d57ebeb0ef133da08221
Dell <Dell@DESKTOP-EKCSEO5>
2024-02-08 21:46:26 -0300

    HandleClick

--- bancofront/src/App.js
+++ bancofront/src/App.js
@@ -1,20 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Boton } from "./components/boton";
 import * as APITransferencia from "./services/transferencia";
-import LoginForm from "./Components/LoginForm/LoginForm";
+import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
+  const handleClick = () => {
+    APITransferencia.getTransferencias(1);
+  };
+
   return (
     <div>
+      <Boton
+        className="boton-grande"
+        accion={handleClick}
+        nombreAccion="Ver actividad"
+      />
       <LoginForm />
-      <div>
-        <Boton
-          className="boton-grande"
-          accion={APITransferencia.getTransferencias(1)}
-          nombreAccion="Ver actividad"
-        />
-      </div>
     </div>
   );
 }
