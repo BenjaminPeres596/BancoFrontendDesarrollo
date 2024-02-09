2195a4aa2173f0719c99c6e479866280f6853443
Dell <Dell@DESKTOP-EKCSEO5>
2024-02-09 15:09:58 -0300

    Lista de arrays (nombre de lo que se va a mostrar, array que se le pasa, array de atributos que se van a mostrar)

--- bancofront/src/App.js
+++ bancofront/src/App.js
@@ -1,11 +1,27 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Boton } from "./components/boton";
 import * as APITransferencia from "./services/transferencia";
 import LoginForm from "./components/LoginForm/LoginForm";
+import { ListarArrays } from "./components/listarArrays";
 
 function App() {
+  const [transferencias, setTransferencias] = useState([]);
+  const [mostrarTransferencias, setMostrarTransferencias] = useState(false);
+
   const handleClick = () => {
-    APITransferencia.getTransferencias(1);
+    if (!mostrarTransferencias) {
+      APITransferencia.getTransferencias(1)
+        .then((data) => {
+          setTransferencias(data.datos);
+          setMostrarTransferencias(true);
+        })
+        .catch((error) => {
+          console.error("Error al obtener las transferencias:", error);
+        });
+    } else {
+      setMostrarTransferencias(false);
+    }
   };
 
   return (
@@ -15,6 +31,13 @@ function App() {
         accion={handleClick}
         nombreAccion="Ver actividad"
       />
+      {mostrarTransferencias && (
+        <ListarArrays
+          nombre="Transferencias"
+          array={transferencias}
+          atributos={["monto", "fecha"]}
+        />
+      )}
       <LoginForm />
     </div>
   );
