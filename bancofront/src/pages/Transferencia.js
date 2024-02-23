9ae1e8e24f41d547b9f7ef287db4e97a3d63c18f
rafae <rafaegur29>
2024-02-23 14:45:48 -0300

    agrego services de motivo

--- bancofront/src/pages/Transferencia.js
+++ bancofront/src/pages/Transferencia.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import * as APICuenta from "../services/cuenta";
-import * as APITransferencia from "../services/transferencia";
-import { Desplegable } from "../Components/desplegable";
+import * as APIMotivo from "../services/motivo";
 import { Boton } from "../Components/boton";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./principal.css";
 import { useNavigate } from "react-router-dom";
 
 const Transferencia = ({ cuentaId, cliente }) => {
   const [monto, setMonto] = useState("");
+  const [motivos, setMotivos] = useState([]);
   const [cuentasDestino, setCuentasDestino] = useState([]);
   const [cuentaDestino, setCuentaDestino] = useState("");
   const [mensajeError, setMensajeError] = useState("");
@@ -27,35 +24,77 @@ const Transferencia = ({ cuentaId, cliente }) => {
       });
   }, [cliente.dni, cuentaId]);
 
+  useEffect(() => {
+    APIMotivo.GetMotivos()
+      .then((data) => {
+        const motivos = data.datos;
+        setMotivos(motivos);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los motivos:", error);
+      });
+  }, []);
+
   return (
     <div className="container-fluid">
-      <h3>Realizar Transferencia</h3>
-      <p>Monto:</p>
-      <input
-        type="number"
-        value={monto}
-        onChange={(e) => setMonto(e.target.value)}
-      />
-      <p>Cuenta destino:</p>
-      <input
-        type="string"
-        value={cuentaDestino}
-        onChange={(id) => {
-          console.log("Cuenta destino seleccionada:", id);
-          setCuentaDestino(id);
-        }}
-      />
-      {mensajeError && <p style={{ color: "red" }}>{mensajeError}</p>}
-      
-    <div >
-      <Boton nombreAccion="Realizar Transferencia" />
-      <button type="button" onClick={() => navigate("/principal")}>
-        Volver
-      </button>
-    </div>
-      
+      <div className="row">
+        <h3>Realizar Transferencia</h3>
+
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput" className="form-label">
+            Ingrese el monto a transferir
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="$0"
+            value={monto}
+            onChange={(e) => setMonto(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="formGroupExampleInput2" className="form-label">
+            Ingrese el CBU de destino
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="00000000000000000000"
+            value={cuentaDestino}
+            onChange={(e) => setCuentaDestino(e.target.value)}
+          />
+          {mensajeError && <p style={{ color: "red" }}>{mensajeError}</p>}
+        </div>
+        <div className="col-md-4">
+          <label 
+            htmlFor="inputState" 
+            className="form-label">
+            Motivo
+          </label>
+          <select 
+            id="inputState" 
+            className="form-select">
+            <option value="">Seleccionar Motivo</option>
+            {motivos.map((motivo) => (
+              <option key={motivo.id} value={motivo.id}>
+                {motivo.Nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <div>
+        <Boton nombreAccion="Realizar Transferencia" />
+        <button type="button" onClick={() => navigate("/principal")}>
+          Volver
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Transferencia;
+
---
+++ bancofront/src/services/motivo.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = "https://colosal.duckdns.org:15001/BancoGeneracion";
+
+export async function GetMotivos() {
+  try {
+    const response = await axios.get(`=${API_URL}/Motivo/Get`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
